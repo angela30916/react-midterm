105b76cc7fd53fa2e11fed53551ecfcbfb15b584
angela30916 <angela30916@gmail.com>
2020-11-20 14:41:05 +0800

    ✨ Add "Add a card" feature to TaskList component

--- my-app/src/components/TaskList.jsx
+++ my-app/src/components/TaskList.jsx
@@ -2,9 +2,46 @@ import React, { useState } from "react";
 import "./TaskList.css";
 
 function TaskList(props) {
+  const [name, setName] = useState("");
+
+  const [isEditing, setEditing] = useState(false);
+
+  function handleChange(e) {
+    // setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // if (name) {
+    //     props.addTask(name)
+    //     setName('')
+    // }
+  }
+
+  const editingTemplate = (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleChange} />
+      <br />
+      <br />
+      <button type="submit">
+        <span>Add card</span>
+      </button>
+      <button type="button" onClick={() => setEditing(false)}>
+        <span>X</span>
+      </button>
+    </form>
+  );
+
+  const viewTemplate = (
+    <button type="button" onClick={() => setEditing(true)}>
+      <span>＋ Add a card</span>
+    </button>
+  );
+
   return (
     <div className="task" id={props.id}>
       {props.name}
+      <div className="card">{isEditing ? editingTemplate : viewTemplate}</div>
     </div>
   );
 }
